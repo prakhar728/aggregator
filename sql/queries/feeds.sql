e058bc243e86f0598a8265a34d20b3dbfbd2d1b3
Prakhar <prakharojha12@gmail.com>
2023-11-05 23:43:19 +0530

    Added mark Feed as fetched

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -6,3 +6,16 @@ RETURNING *;
 
 -- name: GetFeeds :many
 SELECT * FROM feeds;
+
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at DESC NULLS FIRST
+LIMIT 1;
+
+-- name: MarkFeedAsFetched :one
+UPDATE feeds 
+SET last_fetched_at =NOW(),
+updated_at = NOW()
+WHERE id=$1
+RETURNING *;
